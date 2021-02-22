5bb8ddcf57da52ad65f4f5f4ea2cd078cf7bc52d
hortencia718 <hortenciaecisneros@gmail.com>
2021-02-22 09:38:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import List from './List';
+import data from './data';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(data);
+
+    function clearAllRecords(){
+      setPeople([])
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className="Container">
+        {/* change this to state variable data */}
+      <h2>{data.length} birthday today</h2>
+      {/* pass (people)data to list here */}
+      <List people={data}/>
+      <button onClick={ clearAllRecords}>Clear all</button>
+      </section>
+    </main>
+  )
+    
 }
 
 export default App;
---
+++ src/data.jsx
@@ -0,0 +1,43 @@
+// import React from 'react';
+
+export default [
+
+    {
+    id: 1,
+    name:'Hortencia ',
+    age: 100,
+    image:'https://images.app.goo.gl/9LCRrgNonGbebjM96'
+    },
+
+    {   
+        id: 2,
+        name: 'Mildred',
+        age: 104,
+        image:'https://images.app.goo.gl/Pza28som6voUfm4f7'
+    },
+    {
+        id: 3,
+        name: 'spoons',
+        age: 175,
+        image:'https://images.app.goo.gl/73Tu4DE2Hv34j5nL6'
+    },
+    {
+        id: 4,
+        name: 'Monica ',
+        age: 349,
+        image:'https://images.app.goo.gl/73Tu4DE2Hv34j5nL6'
+    },
+    {
+        id: 5,
+        name: 'Nia ',
+        age: 22,
+        image:'https://images.app.goo.gl/73Tu4DE2Hv34j5nL6'
+    },
+
+    {
+        id: 6,
+        name: 'lucianno',
+        age: 11,
+        image:'https://images.app.goo.gl/73Tu4DE2Hv34j5nL6'
+    },
+]
